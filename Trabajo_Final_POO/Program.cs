a2f0827bcd8d13fe0d44fe7ded8df1072acc0f3f
Perla-Soleith <25041064@itdurango.edu.mx>
2026-06-11 20:53:04 -0600

    Agregar archivos de proyecto.

---
+++ Trabajo_Final_POO/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Trabajo_Final_POO
+{
+    internal class Program
+    {
+
+        //Proyectito de POO jiji
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
